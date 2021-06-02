78a01072f89f8d46e8620f3f17a865c3e3cb8b62
madhavilatha4 <vjkrish.kavya@gmail.com>
2021-06-02 16:09:55 -0400

    this is first commit

--- src/test/java/com/DollarDays/testcases/TC_WishList_003.java
+++ src/test/java/com/DollarDays/testcases/TC_WishList_003.java
@@ -6,21 +6,18 @@ public class TC_WishList_003 extends Baseclass {
 
 	@Test
 	public void wishlistTest() throws InterruptedException 
-<<<<<<< HEAD
+
 	{ 
-=======
+
 	{
->>>>>>> 300276e11d36bf8b5aea5f02c2e984be0c90f7d7
+
 		driver.manage().window().maximize();
 		invokeWishlist();
 		Thread.sleep(1000);
 		if(isAlertPresent()==true) 
-<<<<<<< HEAD
-		{Thread.sleep(1000);
-=======
+
 		{
-			thread.sleep(1000);
->>>>>>> 300276e11d36bf8b5aea5f02c2e984be0c90f7d7
+			Thread.sleep(1000);
 			driver.switchTo().alert().accept();//close Alert
 			logger.warn("Provide wishList name");
 			logger.info("Test pass");
@@ -28,18 +25,19 @@ public class TC_WishList_003 extends Baseclass {
 		}
 		else {
 			logger.info("Test fail");
-		}
-	}
+		
+	}}}
 
 	public boolean isAlertPresent() 
 	{
 		try
-<<<<<<< HEAD
-		{Thread.sleep(1000);
-=======
+
 		{
-			thread.sleep(1000);
->>>>>>> 300276e11d36bf8b5aea5f02c2e984be0c90f7d7
+			Thread.sleep(1000);
+	
+
+		
+
 			driver.switchTo().alert();
 			return true;
 		}catch (Exception e) {
